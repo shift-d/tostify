c9b9737d99aaf3de7158a6360d7bb4afca6d96f7
Shift <shiftkeydeveloper@gmail.com>
2021-03-23 13:42:31 +0300

    chore(tsconfig-decorators): test case

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
 console.log("Project boilerplate");
+
+function testMethodDecorator(): MethodDecorator {
+  return (
+    target: Object,
+    propertyKey: string | symbol,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    descriptor: TypedPropertyDescriptor<any>
+  ) => {
+    console.log(target);
+    console.log(propertyKey);
+    console.log(descriptor);
+    return descriptor;
+  };
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class TestClass {
+  @testMethodDecorator()
+  testMethod() {
+    this.testMethod();
+  }
+}
